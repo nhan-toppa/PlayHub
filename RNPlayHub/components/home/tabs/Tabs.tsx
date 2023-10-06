1d667dccc33dd284755d5286f5aac2bb8316cf78
Le Thanh Nhan <nhan.lethanh@toppa.vn>
2023-10-06 16:46:43 +0700

    modify tabs styles

--- RNPlayHub/components/home/tabs/Tabs.tsx
+++ RNPlayHub/components/home/tabs/Tabs.tsx
@@ -1,25 +1,30 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	useWindowDimensions,
+} from "react-native";
 import { useState } from "react";
 import { COLORS } from "../../../constants";
 
 const Tabs = () => {
 	const tabs: string[] = ["CLOUD", "CONSOLE", "PC"];
 	const [activeTab, setActiveTab] = useState<string>(tabs[0]);
+	const { width: windowWidth } = useWindowDimensions();
 
 	return (
 		<View style={styles.tabContainer}>
+			<View style={styles.activeTabPill} />
 			{tabs.map((tab, index) => (
 				<TouchableOpacity
 					key={index}
-					style={[
-						styles.tabButton,
-						activeTab === tab && styles.activeTab,
-					]}
+					style={styles.tabButton}
 					onPress={() => {
 						setActiveTab(tab);
 					}}
 				>
-					<Text>{tab}</Text>
+					<Text style={styles.tabText}>{tab}</Text>
 				</TouchableOpacity>
 			))}
 		</View>
@@ -28,18 +33,36 @@ const Tabs = () => {
 
 const styles = StyleSheet.create({
 	tabContainer: {
+		backgroundColor: "#5f5f5f",
 		flexDirection: "row",
-		justifyContent: "space-around",
+		justifyContent: "space-evenly",
 		marginTop: 20,
+		borderRadius: 25,
+		padding: 5,
 	},
 	tabButton: {
+		flex: 1,
+		justifyContent: "center",
 		paddingVertical: 8,
-		paddingHorizontal: 15,
-		backgroundColor: "#5f5f5f",
 		borderRadius: 15,
 	},
 	activeTab: {
 		backgroundColor: COLORS.primary,
+	},
+	tabText: {
+		color: "#fff",
+		fontWeight: "bold",
+		textAlign: "center",
+	},
+	activeTabPill: {
+		backgroundColor: COLORS.primary,
+		width: "33%",
+		height: "100%",
+		borderRadius: 25,
+		position: "absolute",
+		top: 5,
+		left: 5,
+		transform: "translateX(125px)",
 	},
 });
 
