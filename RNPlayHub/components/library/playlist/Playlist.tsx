49f79f9956c2447b3faa6692132d87744055f319
Le Thanh Nhan <nhan.lethanh@toppa.vn>
2023-10-10 14:49:20 +0700

    add playlist

--- RNPlayHub/components/library/playlist/Playlist.tsx
+++ RNPlayHub/components/library/playlist/Playlist.tsx
@@ -1,10 +1,40 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
 import React from "react";
+import { data } from "../../../constants";
 
 const Playlist = () => {
 	return (
 		<View style={styles.container}>
-			<Text>Playlist</Text>
+			<Text style={styles.header}>Playlist</Text>
+			<FlatList
+				data={data.games}
+				renderItem={({ item, index }) => {
+					if (index === data.games.length - 1) {
+						return (
+							<>
+								<Image
+									source={{ uri: item.url }}
+									style={[
+										styles.gridItem,
+										{ marginLeft: 10 },
+									]}
+								/>
+								<View style={styles.lastItem} />
+							</>
+						);
+					}
+					return (
+						<Image
+							source={{ uri: item.url }}
+							style={styles.gridItem}
+						/>
+					);
+				}}
+				numColumns={3}
+				contentContainerStyle={styles.gameGrid}
+				columnWrapperStyle={{ justifyContent: "space-between" }}
+				keyExtractor={(item) => item.id.toString()}
+			/>
 		</View>
 	);
 };
@@ -12,9 +42,24 @@ const Playlist = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		// backgroundColor: COLORS.bgDark,
-		// paddingHorizontal: SPACING.generalPaddingHorizontal,
-		// paddingTop: 20,
+		gap: 20,
+	},
+	header: {
+		color: "#fff",
+		fontSize: 26,
+	},
+	gameGrid: {
+		gap: 10,
+	},
+	gridItem: {
+		width: 120,
+		height: 120,
+		borderRadius: 5,
+	},
+	lastItem: {
+		flexGrow: 1,
+		flexShrink: 1,
+		flexBasis: "auto",
 	},
 });
 
