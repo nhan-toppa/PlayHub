ed76073a367ce0d20e6ef6295072a650e6029695
Le Thanh Nhan <nhan.lethanh@toppa.vn>
2023-10-10 16:26:42 +0700

    wrap playlist item inside  TouchableOpacity

--- RNPlayHub/components/library/playlist/Playlist.tsx
+++ RNPlayHub/components/library/playlist/Playlist.tsx
@@ -1,34 +1,58 @@
-import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	Image,
+	FlatList,
+	TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { data } from "../../../constants";
+import { useRouter } from "expo-router";
 
 const Playlist = () => {
-    const numberOfColumns = 3;
+	const router = useRouter();
+	const numberOfColumns = 3;
 	return (
 		<View style={styles.container}>
 			<Text style={styles.header}>Your playlist</Text>
 			<FlatList
 				data={data.games}
 				renderItem={({ item, index }) => {
-					if (index === data.games.length - 1 && data.games.length % numberOfColumns > 1) {
+					if (
+						index === data.games.length - 1 &&
+						data.games.length % numberOfColumns > 1
+					) {
 						return (
 							<>
-								<Image
-									source={{ uri: item.url }}
-									style={[
-										styles.gridItem,
-										{ marginLeft: 10 },
-									]}
-								/>
+								<TouchableOpacity
+									onPress={() =>
+										router.push(`/game-details/${item.id}`)
+									}
+								>
+									<Image
+										source={{ uri: item.url }}
+										style={[
+											styles.gridItem,
+											{ marginLeft: 10 },
+										]}
+									/>
+								</TouchableOpacity>
 								<View style={styles.lastItem} />
 							</>
 						);
 					}
 					return (
-						<Image
-							source={{ uri: item.url }}
-							style={styles.gridItem}
-						/>
+						<TouchableOpacity
+							onPress={() =>
+								router.push(`/game-details/${item.id}`)
+							}
+						>
+							<Image
+								source={{ uri: item.url }}
+								style={styles.gridItem}
+							/>
+						</TouchableOpacity>
 					);
 				}}
 				numColumns={numberOfColumns}
